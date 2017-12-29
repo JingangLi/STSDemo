aece281320b3489643964cee8617776a638bf4fa
JingangLi <ljg_emails@sina.com>
2017-12-29 08:41:20 +0100

    add hello and ut. But ut doesn't work

---
+++ src/main/java/com/ljg/test/didispace/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.ljg.test.didispace.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String index () {
+		return "hello world!";
+	}
+}
